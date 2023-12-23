5be805d3914637306ea9d90afbce0a10ec75c468
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-23 17:47:22 +0100

    projects

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HomeComp } from '../components/Home/HomeComp';
 import { About } from 'components/About/About';
 import { Services } from 'components/Services/Services';
+import { Projects } from 'components/Projects/Projects';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HomeComp />
       <About />
       <Services />
+      <Projects/>
     </>
   );
 };
