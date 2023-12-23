79673b182d50c33673edd1717e2de3d245181c54
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-23 17:49:37 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer class="footer">
+      <div class="footer__container container grid">
+        <ul class="footer__links">
+          <li>
+            <a href="#about" class="footer__link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#services" class="footer__link">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#projects" class="footer__link">
+              Projects
+            </a>
+          </li>
+        </ul>
+        <span class="footer__copy">
+          &#169; All Rights Reserved By <a href="#">Timacode.</a>
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 
