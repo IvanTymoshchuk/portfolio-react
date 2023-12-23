b8540c02b11160211554013a2dd7110d2a002b5a
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-23 17:46:11 +0100

    add about services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
 
 export const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section class="services section">
+      <h2 class="section__title-2">
+        <span>Services.</span>
+      </h2>
+      <div class="services__container container grid">
+        <article class="services__card">
+          <div class="services__border"></div>
+
+          <div class="services__content">
+            <div class="services__icon">
+              <div class="services__box"></div>
+              <i class="ri-code-box-line"></i>
+            </div>
+            <h2 class="services__title">Development</h2>
+            <p class="services__descr">
+              Custom web development tailored to your specifications, designed
+              to provide a flawless user experience.
+            </p>
+          </div>
+        </article>
+        <article class="services__card">
+          <div class="services__border"></div>
+
+          <div class="services__content">
+            <div class="services__icon">
+              <div class="services__box"></div>
+              <i class="ri-smartphone-line"></i>
+            </div>
+            <h2 class="services__title">Mobile App</h2>
+            <p class="services__descr">
+              Design and transform website projects into mobile apps to provide
+              a seamless user experience.
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
