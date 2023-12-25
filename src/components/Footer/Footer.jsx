d19e5b281770b403173f1192dd30f4addaa2a626
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-25 19:40:30 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
-
+import {
+  Footers,
+  FootersContainer,
+  FootersLinks,
+  FootersLink,
+  FootersCopy,
+  FootersCopyLink,
+} from './Footer.styled';
 
 export const Footer = () => {
   return (
-    <footer class="footer">
-      <div class="footer__container container grid">
-        <ul class="footer__links">
+    <Footers class="footer">
+      <FootersContainer class="footer__container container grid">
+        <FootersLinks class="footer__links">
           <li>
-            <a href="#about" class="footer__link">
+            <FootersLink href="#about" class="footer__link">
               About
-            </a>
+            </FootersLink>
           </li>
           <li>
-            <a href="#services" class="footer__link">
+            <FootersLink href="#services" class="footer__link">
               Services
-            </a>
+            </FootersLink>
           </li>
           <li>
-            <a href="#projects" class="footer__link">
+            <FootersLink href="#projects" class="footer__link">
               Projects
-            </a>
+            </FootersLink>
           </li>
-        </ul>
-        <span class="footer__copy">
-          &#169; All Rights Reserved By <a href="#">Timacode.</a>
-        </span>
-      </div>
-    </footer>
+        </FootersLinks>
+        <FootersCopy class="footer__copy">
+          &#169; All Rights Reserved By{' '}
+          <FootersCopyLink href="#">Timacode.</FootersCopyLink>
+        </FootersCopy>
+      </FootersContainer>
+    </Footers>
   );
 };
-
-
