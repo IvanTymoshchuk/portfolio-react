8e528ace112c66580575eaab71128b4fda526958
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-28 16:30:12 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,49 +25,41 @@ export const Header = () => {
   };
 
   return (
-    <Headers class="header" id="header">
-      <Nav class="nav container">
-        <NavLogo href="#" class="nav__logo">
-          <NavLogoCircle class="nav__logo--circle">IT</NavLogoCircle>
-          <NavLogoName class="nav__logo--name">Ivan Tymoshchuk</NavLogoName>
+    <Headers id="header">
+      <Nav>
+        <NavLogo href="#">
+          <NavLogoCircle>IT</NavLogoCircle>
+          <NavLogoName>Ivan Tymoshchuk</NavLogoName>
         </NavLogo>
 
         <NavMenu className={isMenuOpen ? 'show' : ''}>
-          <NavTitle class="nav__title">Menu</NavTitle>
-          <NavName class="nav__name">Ivan</NavName>
-          <NavList class="nav__list">
-            <li class="nav__item">
-              <NavLink href="#home" class="nav__link">
-                Home
-              </NavLink>
+          <NavTitle>Menu</NavTitle>
+          <NavName>Ivan</NavName>
+          <NavList>
+            <li>
+              <NavLink href="#home">Home</NavLink>
             </li>
 
-            <li class="nav__item">
-              <NavLink href="#about" class="nav__link">
-                About Me
-              </NavLink>
+            <li>
+              <NavLink href="#about">About Me</NavLink>
             </li>
 
-            <li class="nav__item">
-              <NavLink href="#projects" class="nav__link">
-                Projects
-              </NavLink>
+            <li>
+              <NavLink href="#projects">Projects</NavLink>
             </li>
 
-            <li class="nav__item">
-              <NavLink href="#contact" class="nav__link">
-                Contact Me
-              </NavLink>
+            <li>
+              <NavLink href="#contact">Contact Me</NavLink>
             </li>
           </NavList>
 
-          <NavClose class="nav__close" id="nav-close" onClick={toggleMenu}>
+          <NavClose onClick={toggleMenu}>
             <IoEyeOffOutline />
           </NavClose>
         </NavMenu>
-        <NavBtn class="nav__buttons">
+        <NavBtn>
           <RiMoonClearLine />
-          <NavTogle class="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+          <NavTogle onClick={toggleMenu}>
             <IoEyeOutline />
           </NavTogle>
         </NavBtn>
