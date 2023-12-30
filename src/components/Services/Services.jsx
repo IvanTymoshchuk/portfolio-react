e29c56e562f664fd9e5dbb192f7e098e5b00ee8a
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-30 17:14:42 +0100

    Update Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { MotionAnimate } from 'react-motion-animate';
 import {
   Section,
   SectionTitle,
@@ -18,39 +18,68 @@ import { LuSmartphone } from 'react-icons/lu';
 export const Services = () => {
   return (
     <Section id="services">
-      <SectionTitle>
-        <Span>Services.</Span>
-      </SectionTitle>
+      <MotionAnimate
+        variant={{
+          hidden: { opacity: 0.2, rotate: -180 },
+          show: {
+            opacity: 0.8,
+            rotate: 0,
+            transition: {
+              repeat: Infinity,
+              duration: 4,
+              repeatDelay: 1,
+              type: 'spring',
+            },
+          },
+        }}
+      >
+        <SectionTitle>
+          <Span>Services.</Span>
+        </SectionTitle>
+      </MotionAnimate>
       <ServicesContainer>
         <ServicesCard>
-          <ServicesBorder></ServicesBorder>
-
-          <ServicesContent>
-            <ServicesIcon>
-              <ServicesBox></ServicesBox>
-              <RiCodeBoxLine className="services__icons" />
-            </ServicesIcon>
-            <ServicesTitleSec>Development</ServicesTitleSec>
-            <ServicesDesr>
-              Custom web development tailored to your specifications, designed
-              to provide a flawless user experience.
-            </ServicesDesr>
-          </ServicesContent>
+          <MotionAnimate
+            animation="scrollFadeOut"
+            reset={true}
+            delay={0.4}
+            speed={1}
+          >
+            <ServicesBorder></ServicesBorder>
+            <ServicesContent>
+              <ServicesIcon>
+                <ServicesBox></ServicesBox>
+                <RiCodeBoxLine className="services__icons" />
+              </ServicesIcon>
+              <ServicesTitleSec>Development</ServicesTitleSec>
+              <ServicesDesr>
+                Custom web development tailored to your specifications, designed
+                to provide a flawless user experience.
+              </ServicesDesr>
+            </ServicesContent>
+          </MotionAnimate>
         </ServicesCard>
-        <ServicesCard>
-          <ServicesBorder></ServicesBorder>
 
-          <ServicesContent>
-            <ServicesIcon>
-              <ServicesBox></ServicesBox>
-              <LuSmartphone className="services__icons" />
-            </ServicesIcon>
-            <ServicesTitleSec>Mobile App</ServicesTitleSec>
-            <ServicesDesr>
-              Design and transform website projects into mobile apps to provide
-              a seamless user experience.
-            </ServicesDesr>
-          </ServicesContent>
+        <ServicesCard>
+          <MotionAnimate
+            animation="scrollFadeOut"
+            reset={true}
+            delay={0.4}
+            speed={1}
+          >
+            <ServicesBorder></ServicesBorder>
+            <ServicesContent>
+              <ServicesIcon>
+                <ServicesBox></ServicesBox>
+                <LuSmartphone className="services__icons" />
+              </ServicesIcon>
+              <ServicesTitleSec>Mobile App</ServicesTitleSec>
+              <ServicesDesr>
+                Design and transform website projects into mobile apps to
+                provide a seamless user experience.
+              </ServicesDesr>
+            </ServicesContent>
+          </MotionAnimate>
         </ServicesCard>
       </ServicesContainer>
     </Section>
